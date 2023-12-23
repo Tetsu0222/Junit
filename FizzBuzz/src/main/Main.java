bb2c54fd02efe542102bf04467083121bda0d0f2
tetsu <root2120@gmail.com>
2023-12-24 08:30:17 +0900

    FizzBuzz業務的改修

--- FizzBuzz/src/main/Main.java
+++ FizzBuzz/src/main/Main.java
@@ -1,13 +1,17 @@
 package main;
 
-import sampletest.SumpleLogic;
+import sampletest.FizzBuzzSample;
 
 public class Main {
 	public static void main( String[] args ) {
-		//FizzBuzzLogic fizzBuzzLogic = new FizzBuzzLogic();
-		//fizzBuzzLogic.start( args );
+//		FizzBuzzLogic fizzBuzzLogic = new FizzBuzzLogic();
+//		fizzBuzzLogic.start( args );
 		
-		SumpleLogic sumpleLogic = new SumpleLogic();
-		sumpleLogic.start();
+//		SumpleLogic sumpleLogic = new SumpleLogic();
+//		sumpleLogic.start(100);
+		
+		FizzBuzzSample sample = new FizzBuzzSample();
+		sample.sample();
+		sample.sample2();
 	}
 }
--- FizzBuzz/src/sampletest/FizzBuzzSample.java
+++ FizzBuzz/src/sampletest/FizzBuzzSample.java
@@ -4,13 +4,34 @@ import java.util.stream.IntStream;
 
 public class FizzBuzzSample {
 	
+	int BuzzNumber = 5;
+	int FizzNumber = 3;
 	
 	public void sample() {
+		
 		IntStream.rangeClosed( 1 , 100 )
-		.mapToObj( number -> ( number % 15 == 0 ) ? "FizzBuzz" : 
-				 ( number % 5 == 0 ) ? "Buzz" : 
-				 ( number % 3 == 0 ) ? "Fizz" : 
+		.mapToObj( number -> ( number % (BuzzNumber * FizzNumber) == 0 ) ? number + "はFizzBuzz" : 
+				 ( number % BuzzNumber == 0 ) ? number + "はBuzz" : 
+				 ( number % FizzNumber == 0 ) ? number + "はFizz" : 
 				   String.valueOf(number) )
 		.forEach( System.out :: println );
+	}
+	
+	public void sample2() {
+		
+		for( int i = 1 ; i < 100 ; i++ ) {
+			
+			String str = String.valueOf(i);
+			int count = 0; //「～はBuzz」出力時、切り分けに使用
+			
+			if( i % BuzzNumber == 0) {
+				str +=  "はFizz";
+				count++; //FizzBuzzになる際、「はFizzはBuzz」とならないようにする。
+			}
+			if( i % BuzzNumber == 0) {
+				str += count == 0 ? "はBuzz" : "Buzz"; //FizzBuzzかBuzzかの切り分け
+			}
+			System.out.println(str);
+		}
 	}
 }
